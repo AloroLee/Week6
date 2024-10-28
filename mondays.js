10a921266377a2aab5e0427ec8ffce60ebec9663
aloro lee <alorotams@gmail.com>
2024-10-28 10:09:07 -0400

    first commit

---
+++ mondays.js
@@ -0,0 +1,12 @@
+
+//var
+const fruit = ["apple", "banana", "cherry"];
+
+fruit.forEach(f => console.log(f));
+
+//no var
+[999,444,443].forEach(n => console.log(n));
+
+["apple", "banana", "cherry"].forEach((f,i,a) => console.log(i+1, f, a));
+
+[11,14,16,25].forEach((n,i,a) => i ? console.log(n - a[i-1]): 0);
