4c3493a02a3ba041799aef453fcecfc4685a948a
aloro lee <alorotams@gmail.com>
2024-10-28 23:51:38 -0400

    exercise

--- elements/elements.js
+++ elements/elements.js
@@ -1,16 +1,28 @@
 
 const monsters = [
-    {name: "Frank Monster"},
-    {name: "The Mummy"},
-    {name: "vlad the Impaler (Dracula)"}
+    { name: "Frankenstein's Monster" },
+    { name: "The Mummy" },
+    { name: "Vlad the Impaler (Dracula)" }
 ];
 
-function getMonster(m, i){
+function getMonster(m, i) {
     // `<div id="1">The Mummy</div>`
-    return`<div id="${i}">${m.name}</div>`
+    return `<div id="${i}">${m.name}</div>`
 }
 
-// .map()
-console.log(monsters.map(getMonster));
+//.map() transforms input array into output array
+console.log(monsters.map(getMonster).join('\n'));
 
+const numbers = [1, 2, 3, 4];
+console.log(numbers);
+
+//separator
+const separator = "."; //GLUE
+console.log(numbers.join(separator));
+const text = "255.255.0.1";
+
+const delimiter_AKA_splitter = ".";
+const parsed = text.split(delimiter_AKA_splitter);
+console.log(parsed)
+console.log(parsed.join("-"));
 
